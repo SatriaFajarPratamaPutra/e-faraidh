aa21629a8070d13509f3ddab62d5be0a92ecff67
Rama <ramasgp10@gmail.com>
2023-11-04 03:52:21 +0700

    Buat page login bagian activity_login_page.xml dan LoginPageActivity.kt, menambahi dokumentasi komentar pada splash screen dan on boarding, belum yang login page, menambahi file LoginPageActivity pada file AndroidManifest.xml

--- app/src/main/java/com/example/splash/LoginPageActivity.kt
+++ app/src/main/java/com/example/splash/LoginPageActivity.kt
@@ -1,12 +1,11 @@
 package com.example.splash
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class OnboardingActivity : AppCompatActivity() {
+class LoginPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding) // Mengatur tata letak untuk aktivitas onboarding
+        setContentView(R.layout.activity_login_page) // Mengatur tata letak untuk aktivitas login page
     }
 }
--- app/src/main/java/com/example/splash/MainActivity.kt
+++ app/src/main/java/com/example/splash/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.splash
+
 import android.os.Handler
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private val SPLASH_TIMEOUT: Long = 3000
+    private val SPLASH_TIMEOUT: Long = 3000 // Waktu tampilan splash screen (dalam milidetik)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_splash_screen) // Mengatur tata letak untuk aktivitas splash screen
+
+        // Handler digunakan untuk menunda proses selama SPLASH_TIMEOUT (3000 milidetik atau 3 detik).
         Handler().postDelayed({
+            // Setelah waktu SPLASH_TIMEOUT berlalu, buat Intent untuk memulai aktivitas berikutnya.
             val intent = Intent(this, OnboardingActivity::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(intent) // Memulai aktivitas OnboardingActivity.
+            finish() // Menyelesaikan aktivitas splash screen agar tidak dapat diakses kembali setelah berpindah ke aktivitas berikutnya.
         }, SPLASH_TIMEOUT)
     }
 }
