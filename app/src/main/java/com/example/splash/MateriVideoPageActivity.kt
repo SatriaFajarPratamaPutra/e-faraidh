d38309c047346aa0ee9222cd3cf6a093b78dc5ab
Rama <ramasgp10@gmail.com>
2023-11-05 01:45:46 +0700

    membuat MateriVideoPageActivity.kt dan activity_materivideo_page.xml, lalu membuat juga activity_onboarding.xml, dan mengedit di AndroidManifest.xml. Serta menambahkan beberapa file drawable, aku ngehapus file file yang kemarin aku upload manual di github seperti eye, icon, icon2 dan line, ini aku upload semua nya ulang

---
+++ app/src/main/java/com/example/splash/MateriVideoPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MateriVideoPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materivideo_page) // Mengatur tata letak untuk aktivitas materi video page
+    }
+}
