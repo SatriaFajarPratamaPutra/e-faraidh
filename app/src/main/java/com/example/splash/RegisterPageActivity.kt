4ed2c55e83e9a55c7259a104b30d1fd092282577
Rama <ramasgp10@gmail.com>
2023-11-04 16:37:24 +0700

    buat register page .kt dan .xml nya, lalu konfigurasi AndroidManifest.xml agar bisa di run, lalu juga menambah-nambahkan file icon drawable

---
+++ app/src/main/java/com/example/splash/RegisterPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page) // Mengatur tata letak untuk aktivitas register page
+    }
+}
