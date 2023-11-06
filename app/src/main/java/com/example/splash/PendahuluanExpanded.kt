50410088b0f420808a2e0e700929f1b715750f01
Rama <ramasgp10@gmail.com>
2023-11-06 15:00:19 +0700

    membuat file activity_pendahuluan_expanded_page.xml untuk PendahuluanExpanded.kt, juga menambahkan di AndroidManifest.xml, dan menambahkan beberapa file

---
+++ app/src/main/java/com/example/splash/PendahuluanExpanded.kt
@@ -0,0 +1,12 @@
+package com.example.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PendahuluanExpanded : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pendahuluan_expanded_page) // Mengatur tata letak untuk aktivitas pendahuluan expanded
+    }
+}
