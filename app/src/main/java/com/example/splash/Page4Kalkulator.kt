d3302f1d5a3cb150304ad21c5ed803f508befd89
Rizky <rizkydwi22purnomo@gmail.com>
2023-11-30 00:34:02 +0700

    Membenarkan .xml nya kalkulator3 dan kalkulator4

--- app/src/main/java/com/example/splash/Page4Kalkulator.kt
+++ app/src/main/java/com/example/splash/Page4Kalkulator.kt
@@ -7,24 +7,24 @@ import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
-class Page3Kalkulator : AppCompatActivity() {
+class Page4Kalkulator : AppCompatActivity() {
     private lateinit var imgbutton1: ImageButton
     private lateinit var button1: Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kalkulator_page3) // Mengatur tata letak untuk aktivitas pendahuluan expanded
+        setContentView(R.layout.activity_kalkulator_page4) // Mengatur tata letak untuk aktivitas pendahuluan expanded
 
         imgbutton1 = findViewById(R.id.arrowback)
         imgbutton1.setOnClickListener {
-            val intent = Intent(this, Page2Kalkulator::class.java)
+            val intent = Intent(this, Page3Kalkulator::class.java)
             startActivity(intent)
             finish()
         }
 
         button1 = findViewById(R.id.selanjutnya)
         button1.setOnClickListener {
-            val intent = Intent(this, Page4Kalkulator::class.java)
+            val intent = Intent(this, Page5Kalkulator::class.java)
             startActivity(intent)
             finish()
         }
