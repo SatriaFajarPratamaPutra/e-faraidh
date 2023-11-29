f0641ae05d4e9dd5c04a4a4081e5c06ed35aa27c
Rama <ramasgp10@gmail.com>
2023-11-30 03:09:24 +0700

    p

--- app/src/main/java/com/example/splash/Page4Kalkulator.kt
+++ app/src/main/java/com/example/splash/Page4Kalkulator.kt
@@ -3,6 +3,9 @@ package com.example.splash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -28,5 +31,21 @@ class Page4Kalkulator : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        setTouchListener()
+    }
+
+    private fun setTouchListener() {
+        val view = findViewById<View>(android.R.id.content)
+        view.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboard()
+            }
+            false
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
     }
 }
